8ea09d6a89ec9bb58b70ca739aeea562d201e840
Bo Zhang <bo@gradle.com>
2019-06-02 11:43:38 +0800

    Update problems

--- src/test/java/com/github/hcsp/datatype/MainTest.java
+++ src/test/java/com/github/hcsp/datatype/MainTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class MainTest {
     @Test
     public void testStringAdd() {
-        assertEquals("479", Main.add("123", "456"));
+        assertEquals("579", Main.add("123", "456"));
         assertEquals("0", Main.add("123", "-123"));
         assertEquals("123", Main.add("123", null));
         assertEquals("0", Main.add(null, null));

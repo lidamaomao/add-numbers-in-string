cd6c495ca60265663e5df87442df699058005b70
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:07 +0800

    Update repo

--- src/test/java/com/github/hcsp/datatype/MainTest.java
+++ src/test/java/com/github/hcsp/datatype/MainTest.java
@@ -1,15 +1,14 @@
 package com.github.hcsp.datatype;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class MainTest {
+public class MainTest {
     @Test
     public void testStringAdd() {
-        assertEquals("579", Main.add("123", "456"));
-        assertEquals("0", Main.add("123", "-123"));
-        assertEquals("123", Main.add("123", null));
-        assertEquals("0", Main.add(null, null));
+        Assertions.assertEquals("579", Main.add("123", "456"));
+        Assertions.assertEquals("0", Main.add("123", "-123"));
+        Assertions.assertEquals("123", Main.add("123", null));
+        Assertions.assertEquals("0", Main.add(null, null));
     }
 }

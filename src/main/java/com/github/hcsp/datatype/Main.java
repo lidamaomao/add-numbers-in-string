74409caf3bd0331d31926aef01dd168727097474
ws1603 <48832455+ws1603@users.noreply.github.com>
2019-10-09 19:03:51 +0800

    实现了字符串中的数字相加 (#78)
    实现了字符串中的数字相加 (#78)

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -6,8 +6,22 @@ public class Main {
         System.out.println(add("123", null));
         System.out.println(add(null, null));
     }
+
     // 给出两个数字字符串a和b，返回其中的数字相加后的字符串结果。
     // 例如，给定a="123",b="456"，返回"579"，因为123+456=579
     // 注意，若参数为null，则当作0处理，即add("123", null)=="123", add(null, null)=="0"
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        if (a != null && b != null) {
+            int a1 = Integer.parseInt(a), b1 = Integer.parseInt(b);
+            int c = a1 + b1;
+            return String.valueOf(c);
+        }
+        if (a == null && b == null) {
+            return "0";
+        }
+        if (a == null) {
+            return b;
+        }
+        return a;
+    }
 }

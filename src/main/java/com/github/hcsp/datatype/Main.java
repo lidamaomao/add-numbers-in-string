0aa14fe3a4746337af3ea7b4149273d47f17bedd
linsinan1995 <47880367+linsinan1995@users.noreply.github.com>
2019-10-12 00:36:56 +0200

    add String (#83)
    add String (#83)

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -1,4 +1,4 @@
-package com.github.hcsp.datatype;
+    package com.github.hcsp.datatype;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,5 +9,11 @@ public class Main {
     // 给出两个数字字符串a和b，返回其中的数字相加后的字符串结果。
     // 例如，给定a="123",b="456"，返回"579"，因为123+456=579
     // 注意，若参数为null，则当作0处理，即add("123", null)=="123", add(null, null)=="0"
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        int int_a, int_b;
+        int_a = a == null ? 0 : Integer.parseInt(a);
+        int_b = b == null ? 0 : Integer.parseInt(b);
+
+        return Integer.toString(int_a+int_b);
+    }
 }

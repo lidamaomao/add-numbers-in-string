e4538a1b8fa1c5eaebb3072a2d6466c0b5dd49b1
Chenghao Zheng <45369893+NervousOrange@users.noreply.github.com>
2019-12-19 18:54:21 +0100

    from NervousOrange: Assignment 2 of data type and Autoboxing. (#115)
    from NervousOrange: Assignment 2 of data type and Autoboxing. (#115)

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -9,5 +9,18 @@ public class Main {
     // 给出两个数字字符串a和b，返回其中的数字相加后的字符串结果。
     // 例如，给定a="123",b="456"，返回"579"，因为123+456=579
     // 注意，若参数为null，则当作0处理，即add("123", null)=="123", add(null, null)=="0"
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        int tempA, tempB;
+        if (a == null) {
+            tempA = 0;
+        } else {
+            tempA = Integer.parseInt(a);
+        }
+        if (b == null) {
+            tempB = 0;
+        } else {
+            tempB = Integer.parseInt(b);
+        }
+        return Integer.toString(tempA + tempB);
+    }
 }

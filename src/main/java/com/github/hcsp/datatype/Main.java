8851991d8467fd07b78f3176ce40aebd29cc0c58
yangbisheng <2606424906@qq.com>
2021-08-05 00:26:07 +0800

    add numbers in string (#251)
    add numbers in string (#251)

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -7,7 +7,15 @@ public class Main {
         System.out.println(add(null, null));
     }
     // 给出两个数字字符串a和b，返回其中的数字相加后的字符串结果。
-    // 例如，给定a="123",b="456"，返回"579"，因为123+456=579
+    // 例如，给定a="123",b="456"，返回"579"，+456=579因为123
     // 注意，若参数为null，则当作0处理，即add("123", null)=="123", add(null, null)=="0"
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        if(a == null){
+            a = "0";
+        }
+        if (b == null){
+            b = "0";
+        }
+        return Integer.toString(Integer.parseInt(a) + Integer.parseInt(b));
+    }
 }

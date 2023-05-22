fb461385e028ba43479562ecda6fd9ac7654fc61
spearhead0428 <110843384+spearhead0428@users.noreply.github.com>
2023-05-22 21:00:17 +0800

    1231231 (#308)
    1231231 (#308)

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -9,5 +9,17 @@ public class Main {
     // 给出两个数字字符串a和b，返回其中的数字相加后的字符串结果。
     // 例如，给定a="123",b="456"，返回"579"，因为123+456=579
     // 注意，若参数为null，则当作0处理，即add("123", null)=="123", add(null, null)=="0"
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        Integer value = to_zero(a) + to_zero(b);
+        return String.valueOf(value);
+    }
+    public static int to_zero(String a){
+        int temp_value;
+        if(a == null){
+            temp_value = 0;
+        }else{
+            temp_value = Integer.parseInt(a);
+        }
+        return temp_value;
+    }
 }
